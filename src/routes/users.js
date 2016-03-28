c460b2d9cf87cdb817e3d15a9f870381cdbb9c3d
Daniel Brown <d.t.brown@outlook.com>
2016-03-28 01:27:43 +0100

    Routes now uses User Controller

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import log from '../log';
+import * as users from '../controllers/user-controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/', (req, res) => {
-  res.status(404).json({ error: 'Not yet implemented' });
+  users.getAll()
+    .then(allUsers => {
+      res.json({ users: allUsers });
+    })
+    .catch(error => {
+      log.error({ error }, 'Error getting a list of users');
+      res.status(500).json({ error: 'Could not get list of users' });
+    });
 });
 
 export default router;
