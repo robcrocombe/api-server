122a30a6dcbbb23e7bbb0806f8542e30754e14a0
Daniel Brown <d.t.brown@outlook.com>
2016-04-04 23:00:50 +0100

    Fixed issue with errors not being passed on from controller. Added logging in the controller.

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from '../database/models/user';
+import log from '../log';
 
 function removeNonPublicAttributes(user) {
   const apiSafeUser = user;
@@ -11,7 +12,7 @@ function removeNonPublicAttributes(user) {
 }
 
 export function getAll() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     User.findAll({
       where: { verified: true },
       raw: true
@@ -19,12 +20,16 @@ export function getAll() {
       .then(allUsers => {
         const apiSafeUsers = allUsers.map(user => removeNonPublicAttributes(user));
         resolve(apiSafeUsers);
+      })
+      .catch(error => {
+        log.error({ error }, 'Error getting list of all users');
+        reject(error);
       });
   });
 }
 
 export function getById(id) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) =>
     User.findOne({
       where: {
         id,
@@ -34,6 +39,10 @@ export function getById(id) {
     })
       .then(user => {
         user ? resolve(removeNonPublicAttributes(user)) : resolve(null);
-      });
-  });
+      })
+      .catch(error => {
+        log.error({ error, id }, 'Error getting user by id');
+        reject(error);
+      })
+  );
 }
