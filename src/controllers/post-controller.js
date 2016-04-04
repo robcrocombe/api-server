86804680ca4a3977bb20725efe83e963c86b4d6d
Daniel Brown <d.t.brown@outlook.com>
2016-04-04 23:13:36 +0100

    Fixed errors not being passed from post controller. Added logging to post controller.

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -1,4 +1,5 @@
 import BlogPost from '../database/models/blog-post';
+import log from '../log';
 
 function removeNonPublicAttributes(post) {
   const apiSafePost = post;
@@ -8,23 +9,29 @@ function removeNonPublicAttributes(post) {
 }
 
 export function getAll() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     BlogPost.findAll({
       raw: true
     }).then(allPosts => {
       const apiSafePosts = allPosts.map(post => removeNonPublicAttributes(post));
       resolve(apiSafePosts);
+    }).catch(error => {
+      log.error({ error }, 'Error getting list of all posts');
+      reject(error);
     });
   });
 }
 
 export function getById(id) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     BlogPost.findOne({
       where: { id },
       raw: true
     }).then(post => {
       post ? resolve(removeNonPublicAttributes(post)) : resolve(null);
+    }).catch(error => {
+      log.error({ error }, 'Error getting blog post by id');
+      reject(error);
     });
   });
 }
