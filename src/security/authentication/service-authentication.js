3530eda1e19ba66a27f4f178808e411958022494
Daniel Brown <d.t.brown@outlook.com>
2016-04-30 23:26:20 +0100

    Complete flow for github now works. WordPress and StackExchange getUserInformation() functions written but yet to be tested

--- src/security/authentication/service-authentication.js
+++ src/security/authentication/service-authentication.js
@@ -1,18 +1,86 @@
 import passport from 'passport';
 import { BasicStrategy } from 'passport-http';
+import request from 'request';
 import log from '../../log';
+import User from '../../database/models/user';
+
+function getUserInformationFromGithub(accessToken) {
+  return new Promise((resolve, reject) => {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+      },
+      url: `https://api.github.com/user?access_token=${accessToken}`
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const githubUser = JSON.parse(body);
+        resolve({ authentication_id: githubUser.id.toString(), authentication_provider: 'github' });
+      } else {
+        reject(error || `Non-OK response from Github (${response.statusCode})`);
+      }
+    });
+  });
+}
+
+function getUserInformationFromWordpress(accessToken) {
+  // TODO: Test and fix
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'https://public-api.wordpress.com/rest/v1.1/me',
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      }
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve({ authentication_id: body.ID, authentication_provider: 'wordpress' });
+      } else {
+        reject(error || 'Non-OK response from Wordpress');
+      }
+    });
+  });
+}
+
+function getUserInformationFromStackExchange(accessToken) {
+  // TODO: Test and fix
+  return new Promise((resolve, reject) => {
+    request(`https://api.stackexchange.com/2.2/me/&access_token=${accessToken}`, (error, response, body) => {
+      if (! error && response.statusCode === 200) {
+        resolve({ authentication_id: body.items[0].user_id, authentication_provider: 'stack_exchange' });
+      } else {
+        reject(error || 'Non-OK response from Stack Exchange');
+      }
+    });
+  });
+}
 
 function getUserFromService(authenticationProvider, accessToken) {
+  let userInformationPromise = null;
   switch (authenticationProvider) {
     case 'github':
-      return Promise.resolve({ firstName: 'Danny' });
-    // case 'stack_exchange':
-    //   return getStackExchangeUser(accessToken);
-    // case 'wordpress':
-    //   return getWordpressUser(accessToken);
+      userInformationPromise = getUserInformationFromGithub(accessToken);
+      break;
+    case 'stack_exchange':
+      userInformationPromise = getUserInformationFromStackExchange(accessToken);
+      break;
+    case 'wordpress':
+      userInformationPromise = getUserInformationFromWordpress(accessToken);
+      break;
     default:
       return Promise.reject(new Error('Invalid authentication provider (username)'));
   }
+  return new Promise((resolve, reject) => {
+    userInformationPromise.then(({ authentication_id, authentication_provider }) => {
+      User.findOne({
+        where: {
+          authentication_id,
+          authentication_provider
+        }
+      })
+        .then(user => resolve(user))
+        .catch(error => reject(error));
+    })
+      .catch(error => reject(error));
+  });
 }
 
 export function configureAuthentication() {
