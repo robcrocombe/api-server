5f624708e74ed2c4753def20355f38c4f7c57b6a
Daniel Brown <d.t.brown@outlook.com>
2016-10-21 23:59:00 +0100

    Started work on third party authentication verification

--- src/components/token/token-routes.js
+++ src/components/token/token-routes.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import fetch from 'node-fetch';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+function getGitHubUserDetails(accessToken) {
+  fetch(`https://api.github.com/user?access_token=${accessToken}`)
+    .then(res => res.json())
+    .then(json => ({ authenticationProvider: 'github', authenticationId: json.id }));
+}
+
 router.post('/', (req, res) => {
-  const authentication = {
-    authenticationProvider: 'github',
-    authenticationId: '4321'
-  };
+  let authentication = null;
+
+  switch (req.body.authenticationProvider) {
+    case 'github':
+      authentication = getGitHubUserDetails(res.body.accessToken);
+      break;
+    default:
+      return res.status(422).json({ error: 'Authentication Provider required' });
+  }
+
+  if (!authentication) {
+    return res.status(422).json({ error: `Incorrect ${req.body.authenticationProvider} authentication details provided` });
+  }
 
   const token = jwt.sign(authentication, process.env.CSBLOGS_JWT_SECRET);
 
-  res.json({
+  return res.json({
     token
   });
 });
