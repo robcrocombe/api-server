6ea7ccb769e75cffbaa6ab7629bb9cff61262685
Daniel Brown <d.t.brown@outlook.com>
2016-10-21 19:38:00 +0100

    No idea why this wasn't included in my `authenticate` commit

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,6 +3,25 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import { getByAuthenticationDetails as getUserByAuthenticationDetails } from '../components/user/user-controller';
 
 const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  secretOrKey: process.env.CSBLOGS_JWT_SECRET
 };
 
 passport.use(new JwtStrategy(options, (jwtPayload, done) => {
+  const authenticationProvider = jwtPayload.authenticationProvider;
+  const authenticationId = jwtPayload.authenticationId;
+
+  getUserByAuthenticationDetails(authenticationProvider, authenticationId)
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(error => {
+      done(error, false);
+    });
+}));
+
+export default passport.authenticate('jwt', { session: false });
